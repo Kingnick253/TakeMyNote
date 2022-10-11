eb7480124e2f935e8c079685dbf8688dd4de2084
Kingnick253 <nicholaswebb2016@gmail.com>
2022-10-11 13:18:55 -0700

    Finished adding pseudo code, along with Routes folder and server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+//require express
+const express = require('express');
+const PORT = 3001;
+
+//require the 'db.json" file and store it in 'Notes'
+//Use express to intialize the 'app' server
+
+//GET /notes should return the notes.html file
+
+//GET * should return the index.html file.
+
+//GET /api/notes should read the db.json file
+
+    // res. json()
+
+//POST /api/notes should receive a new note to save on the request body, add add it to the db.json file, and then return the new note to the client.
+
+        // fs.writeFile
+
+
+//Use the 'app' to listen to a specific PORT(3001) will need to change when i deploy to heroku
+
+
