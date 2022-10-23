5dc58045c6aea14924be2bd6869e3a88a0b723b7
Kingnick253 <nicholaswebb2016@gmail.com>
2022-10-22 22:40:48 -0700

    Started new post route to save notes, currently not working but I am consolelogging an error

--- routes/notes-route.js
+++ routes/notes-route.js
@@ -1,14 +1,31 @@
 const router = require('express').Router();
+// requires fs to write file
 const fs = require('fs');
+
 let noteDB = require('../db/db.json');
 
-
-router.get('/', (req,res) =>{
+// GET route to get the notes in the db folder
+router.get('/', (req, res) =>{
     return res.json(noteDB);
 });
 
 
+router.post('/api/notes', (req, res)=>{
+    let notes_db = fs.readFileSync('./db/db.json');
+    notes_db = JSON.parse(notes_db);
+    res.json(notes_db);
 
+    let newNote = {
+        title: req.body.title,
+        text: req.body.text
+    };
+
+    console.log(newNote);
+
+    notes_db.push(newNote);
+    
+    fs.writeFileSync('./db');
+});
 
 
 
